0f8f421d866764ca662bfd422782d085c4a96c52
fffplok <fffplok@gmail.com>
2015-03-31 09:19:14 -0500

    introduce constants, Entity, Game, refactor Engine

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
+//constants
+var ROWS = 6,
+    COLS = 7,
+    TILES_TOP = 10, //for rendering tiles so that entities position better over tile
+    TILE_HEIGHT = 171,
+    ROW_HEIGHT = 83,
+    COL_WIDTH = 101;
+
+var Game = (function() {
+    var level = 0,
+        tiles = [],
+        api = {
+            tiles: tiles,
+
+            init: function() {
+                this.buildTiles();
+                player = new Player('images/char-boy.png');
+                this.setPlayerStart();
+            },
+            setPlayerStart: function() {
+                console.log('setPlayerStart, player.bottommost: ', player.bottommost);
+                player.setPosition(2*COL_WIDTH, player.bottommost);
+            },
+            buildTiles: function() {
+                var row, col,
+                    rowImages = [
+                    'images/water-block.png',   // Top row is water
+                    'images/stone-block.png',   // Row 1 of 3 of stone
+                    'images/stone-block.png',   // Row 2 of 3 of stone
+                    'images/stone-block.png',   // Row 3 of 3 of stone
+                    'images/grass-block.png',   // Row 1 of 2 of grass
+                    'images/grass-block.png'    // Row 2 of 2 of grass
+                ];
+
+                for (row = 0; row < ROWS; row++) {
+                    for (col = 0; col < COLS; col++) {
+                        tiles.push(new Entity(rowImages[row], col * COL_WIDTH, row * ROW_HEIGHT + TILES_TOP));
+                    }
+                }
+            }
+        };
+
+    return api;
+})();
+
+//All objects placed on the canvas may be a subclass of Entity.
+// each object has a sprite and x and y position. Each object can be rendered.
+var Entity = function(img, x, y) {
+    this.sprite = img;
+    this.x = x || 0;
+    this.y = y || 0;
+};
+
+Entity.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Entity.prototype.setPosition = function(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,6 +68,10 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    //images are 101x171, but overlap, from resources, find 83 as row interval
+    this.x = 0;
+    this.y = 0;
 }
 
 // Update the enemy's position, required method for game
@@ -25,11 +91,60 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+//GG - Player does not need to have prototypal inheritance as there
+// is only one instance. Begin by copying Enemy, later may implement
+// module pattern.
+var Player = function(img) {
+    Entity.call(this, img);
 
+    this.rightmost = (COLS - 1) * COL_WIDTH;
+    this.bottommost = (ROWS - 1) * ROW_HEIGHT;
+}
+
+Player.prototype = Object.create(Entity.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function(dt) {
+    //Signature here, just one dt?
+}
+
+Player.prototype.handleInput = function(k) {
+    //console.log('Player.handleInput, k:', k);
+    switch(k) {
+        case 'up':
+            //y must be between 0 and 405 (83 x number of rows-1), y offset is -10
+            if (this.y >= ROW_HEIGHT) this.y -= ROW_HEIGHT;
+            break;
+        case 'down':
+            if (this.y < this.bottommost) this.y += ROW_HEIGHT;
+            break;
+        case 'left':
+            //x must be between 0 and 404 (101 x number of cols-1)
+            if (this.x >= COL_WIDTH) this.x -= COL_WIDTH;
+            break;
+        case 'right':
+            if (this.x < this.rightmost) this.x += COL_WIDTH;
+            break;
+        default:
+            console.log('unknown keycode, k:', k);
+    }
+    //Signature here?
+}
+//var matrix = []; //would this be useful?
+var allEnemies;
+var buildEnemies = function(n) {
+    allEnemies = []; //each call to buildEnemies refreshes enemies with new array
+    for (var i = 0; i < n; i++) {
+        allEnemies.push(new Enemy());
+    }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+Game.init();
+//var player = new Player('images/char-boy.png');
+buildEnemies(3);
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -14,6 +14,7 @@
  * a little simpler to work with.
  */
 
+//GG note. there is no point in declaring a variable Engine here. It remains undefined because the closure doesn't return anything.
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
@@ -25,8 +26,8 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    canvas.width = COLS * COL_WIDTH; //707; //505;
+    canvas.height = ROWS * ROW_HEIGHT + TILE_HEIGHT; // - ROW_HEIGHT; // //606;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -64,8 +65,10 @@ var Engine = (function(global) {
      * game loop.
      */
     function init() {
+        console.log('Game:',Game);
         reset();
         lastTime = Date.now();
+
         main();
     }
 
@@ -104,40 +107,17 @@ var Engine = (function(global) {
      * they are just drawing the entire screen over and over.
      */
     function render() {
-        /* This array holds the relative URL to the image used
-         * for that particular row of the game level.
-         */
-        var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
-            ],
-            numRows = 6,
-            numCols = 5,
-            row, col;
-
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
-         */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                /* The drawImage function of the canvas' context element
-                 * requires 3 parameters: the image to draw, the x coordinate
-                 * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
-                 */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-            }
-        }
-
-
+        renderBoard();
         renderEntities();
+    }
+
+    /* GG let's leave open the possibility that the board will change. Game exposes the tiles array, each tile
+     * is an instance of Entity and will render itself.
+     */
+    function renderBoard() {
+        Game.tiles.forEach(function(tile) {
+            tile.render();
+        });
     }
 
     /* This function is called by the render function and is called on each game
@@ -181,4 +161,9 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
+
+    //GG note. if we wanted a variable Engine, we need to return something
+    return {
+        render: render
+    }
 })(this);
